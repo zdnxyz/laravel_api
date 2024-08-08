b159d6c42ca0b7918f52aaf39f05b7dd92e75540
zdnxyz <zidanahmad2343@gmail.com>
2024-08-08 10:25:51 +0700

    sanctum

--- routes/api.php
+++ routes/api.php
@@ -9,14 +9,14 @@ use Illuminate\Http\Request;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-// })->middleware('auth:sanctum');
+})->middleware('auth:sanctum');
 
 // Route::middleware((['auth:sanctum']))->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::resource('kategori', Kategoricontroller::class);
     Route::resource('genre', Genrecontroller::class);
     Route::resource('aktor', Aktorcontroller::class);
-});
+// });
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
